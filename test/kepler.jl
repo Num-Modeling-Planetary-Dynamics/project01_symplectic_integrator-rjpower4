27306eb28c7569610d1d3f48160f67b96797ac49
Rolfe Power <rpower@purdue.edu>
2022-10-19 22:14:29 -0400

    Add test for kepler solver

--- test/kepler.jl
+++ test/kepler.jl
@@ -1,4 +1,4 @@
-using Symple: kepler_danby
+using Symple: kepler_danby, LagrangianState, mean_motion, kepler_propagate, KeplerianElements, period
 using Test
 
 @test all(x -> first(kepler_danby(x, π)) ≈ π, 0.1:0.05:0.9)
@@ -14,4 +14,13 @@ let (result, did_converge) = kepler_danby(0.9, 0.3)
     @test did_converge
 end
 
-
+let gm = 398600.4354360959
+    r = 10_000.0
+    v = 4.9332898193068
+    n = mean_motion(gm, r)
+    p = 2π / n
+    x0 = LagrangianState(r, 0.0, 0.0, 0.0, v, 0.0)
+    els = KeplerianElements(gm, x0)
+    final = kepler_propagate(gm, period(gm, els), x0)
+    @test x0 ≈ final
+end
