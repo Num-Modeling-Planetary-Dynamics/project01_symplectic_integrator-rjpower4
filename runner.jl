29d290880cd1dbe27be001a79c6bd037b5b37d30
Rolfe Power <rpower@purdue.edu>
2022-10-24 12:31:35 -0400

    Runner updates

--- runner.jl
+++ runner.jl
@@ -1,7 +1,7 @@
 using Symple
 using DataFrames
 
-function run()
+function runme()
     n_years = 100_000
 
     start_epoch = "OCT 17 2022"
@@ -16,7 +16,7 @@ function run()
     period_neptune = Symple.period(bodies[1].gm, ke_neptune)
     dt = period_neptune / 30
 
-    simulation = Symple.Simulation((start_et, final_et), bodies; max_step=dt)
+    simulation = Symple.Simulation((start_et, final_et), bodies; max_step = dt)
 
     dem_states = Symple.democratic_heliocentric(bodies, init_lag_states)
 
@@ -28,23 +28,21 @@ function run()
     ham_states = similar(simulation.states)
     lag_states = Matrix{Symple.LagrangianState}(undef, size(ham_states)[1], length(bodies))
     for k in 1:size(ham_states)[1]
-        ham_states[k, :] = Symple.from_democratic_heliocentric(bodies, simulation.states[k, :])
+        ham_states[k, :] = Symple.from_democratic_heliocentric(bodies,
+                                                               simulation.states[k, :])
 
         lag_states[k, :] .= Symple.LagrangianState.(bodies, ham_states[k, :])
     end
 
-    t = range(start_et, final_et; length=size(ham_states)[1])
+    t = range(start_et, final_et; length = size(ham_states)[1])
     energy = Symple.system_energy(simulation)
 
-
-    out = DataFrame(
-        :t => t,
-        :x_sun => lag_states[:,1],
-        :x_neptune => lag_states[:, 2],
-        :x_pluto => lag_states[:, 3],
-        :energy => energy,
-    )
+    out = DataFrame(:t => t,
+                    :x_sun => lag_states[:, 1],
+                    :x_neptune => lag_states[:, 2],
+                    :x_pluto => lag_states[:, 3],
+                    :energy => energy)
 
     # t, Symple.specific_energy.(Ref(Symple.gravitational_parameter(bodies[1])), lag_states[:, 3]), simulation
-    return  out
+    return out
 end
